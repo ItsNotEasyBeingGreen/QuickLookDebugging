ce2fc8bea44440e0bd3c7249c1bf535939e694f1
Mark Cornelisse <mark@afrogleap.com>
2019-02-11 21:04:09 +0100

    Initial Commit

---
+++ QuickLookDebugging/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuickLookDebugging
+//
+//  Created by Mark Cornelisse on 11/02/2019.
+//  Copyright © 2019 Mark Cornelisse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
