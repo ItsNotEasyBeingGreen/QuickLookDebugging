7992a124e244e2143f24fb33eb228f65c5e4023a
Mark Cornelisse <mark@afrogleap.com>
2019-02-11 21:08:38 +0100

    - Whatever.

---
+++ QuickLookDebugging/CurlView.swift
@@ -0,0 +1,18 @@
+//
+//  CurlView.swift
+//  QuickLookDebugging
+//
+//  Created by Mark Cornelisse on 11/02/2019.
+//  Copyright © 2019 Mark Cornelisse. All rights reserved.
+//
+
+import UIKit
+
+class CurlView: UIView {
+
+    // MARK: UIView
+    
+    // MARK: UIResponder
+    
+    // MARK: NSObject
+}
--- QuickLookDebugging/ViewController.swift
+++ QuickLookDebugging/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: UIViewController
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    // MARK: UIResponder
+    
+    // MARK: NSObject
 }
 
